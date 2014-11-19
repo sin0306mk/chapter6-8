53e8bc47581f7f08aa829bf97ee8b32f6d5976ec
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-19 10:47:28 +0900

    JSONデータ操作
    JSONデータを配列、辞書クラスへの変換

--- chapter6-8/ViewController.swift
+++ chapter6-8/ViewController.swift
@@ -19,6 +19,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        var jsondata = NSData(contentsOfFile: path!)
+        
+//        let jsonArray = NSJSONSerialization.JSONObjectWithData(jsondata!, options: nil, error: nil) as NSArray
+//        for dat in jsonArray {
+//            var d1 = dat["name"] as String
+//            var d2 = dat["price"] as Int
+//            println("名称:\(d1)　値段:\(d2)")
+//        }
+        let jsonDictionary = NSJSONSerialization.JSONObjectWithData(jsondata!, options: nil, error: nil) as NSDictionary
+        for (key, data) in jsonDictionary{
+            var d1 = data["餅"] as String
+            var d2 = data["醤油"] as String
+            var d3 = data["月見団子"] as String
+            println("\(key)-> \(d1)")
+            println("\(key)-> \(d2)")
+            println("\(key)-> \(d3)")
+        }
+    }
 
 
 }
